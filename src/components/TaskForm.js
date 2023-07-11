6420aeb8029979b75b2af6fd558d6a2d76177ce4
AZ <alexzanfir@gmail.com>
2023-07-11 00:48:48 -0400

    added recurrence controls

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function TaskForm({ addTask }) {
   const [task, setTask] = useState('');
   const [listType, setListType] = useState('need-to-do');
+  const [recurrence, setRecurrence] = useState('once');
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -11,6 +12,7 @@ function TaskForm({ addTask }) {
       id: Date.now().toString(),
       task,
       listType,
+      recurrence,
       status: 'not-completed',
       completionTime: null,
       completed: false,
@@ -18,6 +20,7 @@ function TaskForm({ addTask }) {
 
     setTask('');
     setListType('need-to-do');
+    setRecurrence('once');
   };
 
   return (
@@ -29,11 +32,25 @@ function TaskForm({ addTask }) {
         required
         className="dark:bg-gray-700 dark:text-white p-2 rounded-md w-full mb-2 h-24"
       />
-      <select value={listType} onChange={(e) => setListType(e.target.value)} className="dark:bg-gray-700 dark:text-white p-2 rounded-md w-full mb-2">
-        <option value="need-to-do">Need To Do</option>
-        <option value="want-to-do">Want To Do</option>
-      </select>
-      <button type="submit" className="bg-blue-500 text-white p-2 rounded-md w-full">Add Task</button>
+      <div className="flex justify-between items-end">
+        <div className="w-1/3 mr-2">
+          <label className="block text-sm text-gray-600 dark:text-gray-400 mb-2" htmlFor="listType">List Type</label>
+          <select id="listType" value={listType} onChange={(e) => setListType(e.target.value)} className="dark:bg-gray-700 dark:text-white p-2 rounded-md w-full">
+            <option value="need-to-do">Need To Do</option>
+            <option value="want-to-do">Want To Do</option>
+          </select>
+        </div>
+        <div className="w-1/3 mr-2">
+          <label className="block text-sm text-gray-600 dark:text-gray-400 mb-2" htmlFor="recurrence">Recurrence</label>
+          <select id="recurrence" value={recurrence} onChange={(e) => setRecurrence(e.target.value)} className="dark:bg-gray-700 dark:text-white p-2 rounded-md w-full">
+            <option value="once">Once</option>
+            <option value="1 day">1 Day</option>
+            <option value="1 week">1 Week</option>
+            {/* Add more options as needed */}
+          </select>
+        </div>
+        <button type="submit" className="bg-blue-500 text-white p-2 rounded-md w-1/3">Add Task</button>
+      </div>
     </form>
   );
 }
