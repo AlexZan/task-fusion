795c077cf5f27876237a2b6625fb3b9b34b574ca
lex <alexzanfir@gmail.com>
2023-08-06 21:14:08 -0400

    remove  non reference

--- src/components/ConfigModal.js
+++ src/components/ConfigModal.js
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react';
-import { Fragment, useState } from 'react';
+import { Fragment } from 'react';
 import { FaCog } from 'react-icons/fa';
 import { saveToLocalStorage } from '../utils/localStorage';
 
