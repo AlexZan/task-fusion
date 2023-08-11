a4b954a99d1c4a32b4ef22fb33b3d94fd192a477
lex <alexzanfir@gmail.com>
2023-08-11 17:01:09 -0400

    comment out console logs

--- src/index.js
+++ src/index.js
@@ -11,48 +11,48 @@ root.render(
   </React.StrictMode>
 );
 
-Notification.requestPermission().then(function (permission) {
-  if (permission === 'granted') {
-    console.log('Notification permission granted.');
-    // You can now use the Notification API or the Push API
-  } else {
-    console.log('Unable to get permission to notify.');
-  }
-});
+// Notification.requestPermission().then(function (permission) {
+//   if (permission === 'granted') {
+//     console.log('Notification permission granted.');
+//     // You can now use the Notification API or the Push API
+//   } else {
+//     console.log('Unable to get permission to notify.');
+//   }
+// });
 
-navigator.serviceWorker.addEventListener('controllerchange', function () {
-  console.log('Controller changed');
-});
+// navigator.serviceWorker.addEventListener('controllerchange', function () {
+//   console.log('Controller changed');
+// });
 
 
-navigator.serviceWorker.addEventListener('install', event => {
-  console.log('Service Worker installing.');
-});
+// navigator.serviceWorker.addEventListener('install', event => {
+//   console.log('Service Worker installing.');
+// });
 
-navigator.serviceWorker.addEventListener('activate', event => {
-  console.log('Service Worker activated.');
-});
+// navigator.serviceWorker.addEventListener('activate', event => {
+//   console.log('Service Worker activated.');
+// });
 
-navigator.serviceWorker.addEventListener('controllerchange', event => {
-  console.log('Controller changed:', navigator.serviceWorker.controller);
-});
+// navigator.serviceWorker.addEventListener('controllerchange', event => {
+//   console.log('Controller changed:', navigator.serviceWorker.controller);
+// });
 
 // Register the Service Worker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/serviceWorker.js').then((registration) => {
     // Registration was successful
-    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    // console.log('ServiceWorker registration successful with scope: ', registration.scope);
     // Ensure the service worker takes over the page as soon as possible.
     return navigator.serviceWorker.ready;
   }).then(function (reg) {
-    console.log('Service Worker is ready :^)', reg);
+    // console.log('Service Worker is ready :^)', reg);
     // Inform the service worker to take control ASAP
     if (reg.waiting) {
       reg.waiting.postMessage('skipWaiting');
     }
   }).catch((err) => {
     // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
+    // console.log('ServiceWorker registration failed: ', err);
   });
 }
 
