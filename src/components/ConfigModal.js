81fdcab006dcda8aeb7f138ee9c40514437503de
lex <alexzanfir@gmail.com>
2023-08-14 18:06:37 -0400

    renamed config titles

--- src/components/ConfigModal.js
+++ src/components/ConfigModal.js
@@ -22,15 +22,13 @@ function ConfigModal({ isOpen, onClose, needToDoTime, wantToDoTime, setNeedToDoT
             </Dialog.Title>
             <form onSubmit={handleFormSubmit} className="space-y-4">
               <label className="block text-gray-900 dark:text-white">
-                <span>Need To Do Time (minutes):</span>
+                <span>Productivity (minutes):</span>
                 <input type="number" value={needToDoTime / 60} onChange={e => setNeedToDoTime(e.target.value * 60)} className="mt-1 block w-full dark:bg-gray-700 dark:text-white p-2 rounded-md" />
               </label>
               <label className="block text-gray-900 dark:text-white">
-                <span>Want To Do Time (minutes):</span>
+                <span>Enjoyment (minutes):</span>
                 <input type="number" value={wantToDoTime / 60} onChange={e => setWantToDoTime(e.target.value * 60)} className="mt-1 block w-full dark:bg-gray-700 dark:text-white p-2 rounded-md" />
               </label>
-              <p className="text-sm text-gray-500">Note: Changes will only apply to the next timer. This is intentional as to not disturb the current timer progress. You can manually reset the current timer to apply changes immediately.</p>
-
               <button type="submit" className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Save</button>
             </form>
           </div>
