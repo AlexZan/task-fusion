1ff15c3dbbac846369df9e8c0f43921e6b575993
lex <alexzanfir@gmail.com>
2023-08-14 20:29:48 -0400

    completed tasks component

--- src/components/TaskTimer.js
+++ src/components/TaskTimer.js
@@ -45,7 +45,7 @@ function TaskTimer() {
   const seconds = timeLeft % 60;
 
   return (
-    <div className="text-center p-4 dark:bg-gray-800 rounded-lg mb-4 relative">
+    <div className="text-center p-4 dark:bg-gray-800 rounded-lg mb-2   relative">
       <button onClick={handleConfigOpen} className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 transition-colors duration-200"><FaCog /></button>
       <h2 className="text-2xl">{isNeedToDoTime ? 'Productivity' : 'Enjoyment'}</h2>
       <TaskTimerDisplayControl
