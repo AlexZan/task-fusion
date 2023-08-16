e718ba6154fb3131ff0725e6af28dc31228b785b
lex <alexzanfir@gmail.com>
2023-08-16 10:42:45 -0400

    use reducer

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -1,110 +1,108 @@
-import { useState, useEffect } from 'react';
+import { useReducer, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-
 import { saveToLocalStorage, loadFromLocalStorage } from '../utils/localStorage';
 import tasksData from '../place-holder-data.json';
 
-export default function useTasks() {
-  const [isTrackingMode, setTrackingMode] = useState(false);
+const initialState = {
+  isTrackingMode: false,
+  activeTasks: loadFromLocalStorage('activeTasks') ||
+    tasksData.tasks.map((task, index) => ({ ...task, originalIndex: index, isCompleted: false, timeSpent: 0 })),
+  completedTasks: loadFromLocalStorage('completedTasks') || [],
+};
 
-  // Only loading the tasks array from the JSON file
-  const [activeTasks, setActiveTasks] = useState(
-    loadFromLocalStorage('activeTasks') ||
-    tasksData.tasks.map((task, index) => ({ ...task, originalIndex: index, isCompleted: false, timeSpent: 0 }))
-  );
+const actionTypes = {
+  ENTER_TRACKING_MODE: 'ENTER_TRACKING_MODE',
+  EXIT_TRACKING_MODE: 'EXIT_TRACKING_MODE',
+  ADD_TASK: 'ADD_TASK',
+  MOVE_TASK: 'MOVE_TASK',
+  COMPLETE_TASK: 'COMPLETE_TASK',
+  UNCOMPLETE_TASK: 'UNCOMPLETE_TASK',
+  DELETE_TASK: 'DELETE_TASK',
+  UPDATE_TIME_SPENT: 'UPDATE_TIME_SPENT',
+};
 
-  const [completedTasks, setCompletedTasks] = useState(
-    loadFromLocalStorage('completedTasks') || []
-  );
-
-  useEffect(() => {
-    saveToLocalStorage('activeTasks', activeTasks);
-    saveToLocalStorage('completedTasks', completedTasks);
-  }, [activeTasks, completedTasks]);
-
-  const addTask = (task) => {
-    const newTask = {
-      id: uuidv4(), // Generate a unique UUID
-      ...task,
-    };
-    setActiveTasks((prevTasks) => [newTask, ...prevTasks]);
-  };
-
-  const moveTask = (fromIndex, toIndex) => {
-    setActiveTasks((prevTasks) => {
-      const newTasks = [...prevTasks];
+const tasksReducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.ENTER_TRACKING_MODE:
+      return { ...state, isTrackingMode: true };
+    case actionTypes.EXIT_TRACKING_MODE:
+      return { ...state, isTrackingMode: false };
+    case actionTypes.ADD_TASK:
+      const newTask = { id: uuidv4(), ...action.payload };
+      return { ...state, activeTasks: [newTask, ...state.activeTasks] };
+    case actionTypes.MOVE_TASK:
+      const { fromIndex, toIndex } = action.payload;
+      const newTasks = [...state.activeTasks];
       const draggedTask = newTasks[fromIndex];
-      newTasks.splice(fromIndex, 1); // remove the dragged task
-      newTasks.splice(toIndex, 0, draggedTask); // insert the dragged task at the new position
-      return newTasks;
-    });
-  };
-
-  const completeTask = (id) => {
-    const index = activeTasks.findIndex((task) => task.id === id);
-    const taskToComplete = activeTasks[index];
-    setCompletedTasks((prevCompletedTasks) => [
-      taskToComplete,
-      ...prevCompletedTasks,
-    ]);
-    setActiveTasks((prevActiveTasks) => prevActiveTasks.filter((_, i) => i !== index));
-  };
-
-  const uncompleteTask = (id) => {
-    const index = completedTasks.findIndex((task) => task.id === id);
-    const taskToUncomplete = completedTasks[index];
-    setActiveTasks((prevActiveTasks) => [taskToUncomplete, ...prevActiveTasks]);
-    setCompletedTasks((prevCompletedTasks) => prevCompletedTasks.filter((_, i) => i !== index));
-  };
-
-  const handleTaskDeletion = (id) => {
-    setActiveTasks((prevTasks) =>
-      prevTasks.filter((task) => task.id !== id)
-    );
-    setCompletedTasks((prevTasks) =>
-      prevTasks.filter((task) => task.id !== id)
-    );
-  };
-
-  const enterTrackingMode = () => {
-    setTrackingMode(true);
-  };
-
-  const exitTrackingMode = () => {
-    setTrackingMode(false);
-  };
-
-  const updateTimeSpentOnTask = (timeSpent) => {
-    setActiveTasks((prevTasks) => {
-      const newTasks = prevTasks.map((task) => {
-        if (task === getCurrentTask()) {
-          return { ...task, timeSpent: (task.timeSpent || 0) + timeSpent };
+      newTasks.splice(fromIndex, 1);
+      newTasks.splice(toIndex, 0, draggedTask);
+      return { ...state, activeTasks: newTasks };
+    case actionTypes.COMPLETE_TASK:
+      const indexToComplete = state.activeTasks.findIndex((task) => task.id === action.payload);
+      const taskToComplete = state.activeTasks[indexToComplete];
+      return {
+        ...state,
+        completedTasks: [taskToComplete, ...state.completedTasks],
+        activeTasks: state.activeTasks.filter((_, i) => i !== indexToComplete),
+      };
+    case actionTypes.UNCOMPLETE_TASK:
+      const indexToUncomplete = state.completedTasks.findIndex((task) => task.id === action.payload);
+      const taskToUncomplete = state.completedTasks[indexToUncomplete];
+      return {
+        ...state,
+        completedTasks: state.completedTasks.filter((_, i) => i !== indexToUncomplete),
+        activeTasks: [taskToUncomplete, ...state.activeTasks],
+      };
+    case actionTypes.DELETE_TASK:
+      return {
+        ...state,
+        activeTasks: state.activeTasks.filter((task) => task.id !== action.payload),
+        completedTasks: state.completedTasks.filter((task) => task.id !== action.payload),
+      };
+    case actionTypes.UPDATE_TIME_SPENT:
+      const updatedTasks = state.activeTasks.map((task) => {
+        if (task === action.payload.task) {
+          return { ...task, timeSpent: (task.timeSpent || 0) + action.payload.timeSpent };
         }
         return task;
       });
-      console.log("Updated tasks:", newTasks); // Debug log
-      return newTasks;
-    });
-  };
-  
-  const getCurrentTask = () => {
-    // Ensure that this computation is based on the latest state
-    return activeTasks.find((task) => !task.isCompleted);
-  };
-  
+      return { ...state, activeTasks: updatedTasks };
+    default:
+      return state;
+  }
+};
+
+export default function useTasks() {
+  const [state, dispatch] = useReducer(tasksReducer, initialState);
+
+  useEffect(() => {
+    saveToLocalStorage('activeTasks', state.activeTasks);
+    saveToLocalStorage('completedTasks', state.completedTasks);
+  }, [state.activeTasks, state.completedTasks]);
+
+  const enterTrackingMode = () => dispatch({ type: actionTypes.ENTER_TRACKING_MODE });
+  const exitTrackingMode = () => dispatch({ type: actionTypes.EXIT_TRACKING_MODE });
+  const addTask = (task) => dispatch({ type: actionTypes.ADD_TASK, payload: task });
+  const moveTask = (fromIndex, toIndex) => dispatch({ type: actionTypes.MOVE_TASK, payload: { fromIndex, toIndex } });
+  const completeTask = (id) => dispatch({ type: actionTypes.COMPLETE_TASK, payload: id });
+  const uncompleteTask = (id) => dispatch({ type: actionTypes.UNCOMPLETE_TASK, payload: id });
+  const deleteTask = (id) => dispatch({ type: actionTypes.DELETE_TASK, payload: id });
+  const updateTimeSpent = (task, timeSpent) => dispatch({ type: actionTypes.UPDATE_TIME_SPENT, payload: { task, timeSpent } });
+
+  const getCurrentTask = () => state.activeTasks.find((task) => !task.isCompleted);
 
   return {
-    activeTasks,
-    completedTasks,
+    activeTasks: state.activeTasks,
+    completedTasks: state.completedTasks,
     completeTask,
     uncompleteTask,
     addTask,
-    handleTaskDeletion,
+    deleteTask,
     moveTask,
-    isTrackingMode,
+    isTrackingMode: state.isTrackingMode,
     enterTrackingMode,
     exitTrackingMode,
     getCurrentTask,
-    updateTimeSpentOnTask,
+    updateTimeSpent,
   };
 }
