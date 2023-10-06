9b9e324b10cfd3f9963aea43a7ac6097286772a7
lex <alexzanfir@gmail.com>
2023-10-06 14:26:00 -0400

    show beta version

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function Header() {
       <header className="text-center padding-small relative">
         <Link to="/">
           <img src={process.env.PUBLIC_URL + '/logo-thin.PNG'} alt="Logo" className="mx-auto w-1/2 max-w-xs" />
-          Beta
+          Beta v0.1
         </Link>
         <Link to="/insights" className="absolute top-1/2 right-4 transform -translate-y-1/2 text-gray-400 hover:text-white">
           Insights
